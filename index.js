138c28fe5e9fd0d5613ca907469caa0c3568229c
Cartwheel <cartwheel@MBP-Cartwheel.Dlink>
2021-06-08 23:11:06 +0300

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const config = require('./config.json');
+
+let client = new Discord.Client();
+client.login(config.BOT_TOKEN);
+
+const PREFIX = '-pause'
+
+function getText(number) {
+    return 'game is pausing in ' + number;
+}
+
+client.on("message", message => {
+    if(!message.content.startsWith(PREFIX)) return;
+	if(message.author.bot) return;
+    let count = 3;
+    for(let i = 3; i > 0; i--) {
+        setTimeout(function() {
+            message.channel.send(getText(count - i + 1));
+        }, i * 1000);
+       
+    }
+
+    setTimeout(function() {
+        message.channel.send('game is paused')
+    }, 4000);
+    setTimeout(function() {
+        message.channel.send('?')
+    }, 4500)
+
+})
